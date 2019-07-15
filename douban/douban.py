c9805f88adfc9ccbdac5070e0b700890c6d5048c
cf_work_win <365877581@qq.com>
2019-07-15 16:36:48 +0800

    解决数据展示为unicode问题，先josn为python格式数据，再存储，问题解决

--- douban/douban.py
+++ douban/douban.py
@@ -1,16 +1,23 @@
 # encoding: utf-8
-import time
 import requests
-import chardet
-
-_time = int(time.time()*1000)
-headers_app={"User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
+import json
+headers_app = {"User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
              "Referer": "https: // m.douban.com / tv / japanese"}
-url = "https://m.douban.com/rexxar/api/v2/subject_collection/tv_japanese/items?os=ios&for_mobile=1&start=0&count=18&loc_id=108288&_=%d"%_time
+url = "https://m.douban.com/rexxar/api/v2/subject_collection/tv_japanese/items?os=ios&for_mobile=1&start=0&count=18"
 print (url)
-r = requests.get(url,headers=headers_app)
 
-print (r.content.decode().encode('utf-8').decode("unicode_escape"))
+
+
+r = requests.get(url,headers=headers_app)
+data = r.content.decode()
+data_json = json.loads(data)
+print (data_json)
+
+for i in data_json["subject_collection_items"]:
+    with open("豆瓣电视名.txt","a",encoding="utf-8") as f:
+        print (i["title"])
+
+        f.write(i["title"]+"\n")
 #print (r.text.encode('utf-8').decode("unicode_escape"))
 '''
 class DouBan():
